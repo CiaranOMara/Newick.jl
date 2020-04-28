1e3344363fda76e60b143beab7b936e82c789ebf
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2020-04-28 21:06:59 +1000

    Work

--- src/Newick.jl
+++ src/Newick.jl
@@ -8,8 +8,180 @@ import TranscodingStreams: TranscodingStreams, TranscodingStream
 
 using AbstractTrees
 
-include("record.jl")
-include("readrecord.jl")
-include("reader.jl")
+mutable struct Record
+    name::Union{Missing, String}
+    distance::Union{Missing, Float64}
+    parent::Union{Missing, Record}
+    children::Union{Missing, Vector{Record}}
+end
+
+"Root constructor"
+function Record()
+    return Record(missing, missing, missing, missing)
+end
+
+"Child node constructor"
+function Record(parent::Record)
+    return Record(missing, missing, parent, missing)
+end
+
+function prenatal!(record)
+    record.children = Vector{Record}()
+end
+
+function empty!(record::Record)
+    record.name = missing
+    record.distance = missing
+    empty!(record.children)
+    return record
+end
+
+function name(record::Record)
+    return record.name
+end
+
+function name!(record::Record, name)
+    record.name = name
+    return record
+end
+
+function distance(record::Record)
+    return record.distance
+end
+
+function distance!(record::Record, distance)
+    record.distance = distance
+    return record
+end
+
+function haschildren(record::Record)
+    return !ismissing(record.children)
+end
+
+const machine = (function ()
+
+    re = Automa.RegExp
+
+    cat = Automa.RegExp.cat
+    rep = Automa.RegExp.rep
+    opt = Automa.RegExp.opt
+    alt = Automa.RegExp.alt
+
+    name = re"[0-9A-Za-z_-]+?"
+    name.actions[:enter] = [:mark]
+    name.actions[:exit] = [:name]
+
+    distance = let
+        len = cat(re"[0-9]+", opt(re"\.[0-9]*"))
+        len.actions[:enter] = [:mark]
+
+        opt(cat(re":", len))
+    end
+    distance.actions[:exit] = [:distance]
+
+    nodefinish = re","
+    nodefinish.actions[:enter] = [:nodefinish]
+
+    cladestart = re"\("
+    cladestart.actions[:enter] = [:cladestart]
+
+    cladefinish = re"\)"
+    cladefinish.actions[:enter] = [:nodefinish, :cladefinish]
+
+    finish = re";"
+    finish.actions[:enter] = [:finish]
+
+
+    node = cat(
+        opt(cladestart),
+        opt(name),
+        opt(distance),
+        alt(nodefinish, cladestart, cladefinish, finish)
+    )
+
+    newick = rep(node)
+
+    Automa.compile(newick)
+end)()
+
+
+const actions = Dict(
+    :mark => :(@mark),
+
+    :name => quote
+        @debug "name $p"
+        r = @markpos():p - 1
+        if length(r) > 0
+            str = String(data[r])
+            name!(record, str)
+        end
+    end,
+
+    :distance => quote
+        @debug "distance $p"
+        r = @markpos():p - 1
+        if length(r) > 0
+            str = String(data[r])
+            parsed = parse(Float64, str)
+            distance!(record, parsed)
+        end
+    end,
+
+    :nodefinish => quote
+        @debug "nodefinish $p"
+        push!(record.parent.children, record)
+        record = Record(record.parent)
+    end,
+
+    :cladestart => quote
+        @debug "cladestart $p"
+        prenatal!(record)
+        record = Record(record) # Setup first child.
+    end,
+
+    :cladefinish => quote
+        @debug "cladefinish $p"
+        record = record.parent # Move to parent.
+    end,
+
+    :finish => quote
+        @debug "finish $p"
+    end
+)
+
+initcode = quote
+    root = record
+end
+
+returncode = :(return cs, root)
+
+context = Automa.CodeGenContext(generator = :goto, checkbounds = false, loopunroll = 8)
+
+Automa.Stream.generate_reader(
+    :read!,
+    machine,
+    # arguments = (:(record::Record), :(state::Tuple{Int,Int})),
+    arguments = (:(record::Record),),
+    actions = actions,
+    context = context,
+    initcode = initcode,
+    returncode = returncode
+) |> eval
+
+
+function Base.show(io::IO, node::Record)
+    return print(io, (name=node.name, distance=node.distance, children=node.children))
+end
+
+function AbstractTrees.printnode(io::IO, node::Record)
+    return print(io, (name=node.name, distance=node.distance))
+end
+
+function AbstractTrees.children(r::Record)
+    if !haschildren(r)
+        return ()
+    end
+    return r.children
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,57 +2,309 @@ using Test
 using Newick
 using TranscodingStreams
 
-newicks = [
-    "(,,(,));",# 1) no nodes are named
-    "(A,B,(C,D));",# 2) leaf nodes are named
-    "(A,B,(C,D)E)F;",# 3) all nodes are named
-    "(:0.1,:0.2,(:0.3,:0.4):0.5);",# 4) all but root node have a distance to parent
-    "(:0.1,:0.2,(:0.3,:0.4):0.5):0.0;",# 5) all have a distance to parent
-#    00000000011111111112222222222333
-#    12345678901234567890123456789012
-    "(A:0.1,B:0.2,(C:0.3,D:0.4):0.5);",# 6) distances and leaf names (popular)
-    "(A:0.1,B:0.2,(C:0.3,D:0.4)E:0.5)F;",# 7) distances and all names
-    "((B:0.2,(C:0.3,D:0.4)E:0.5)A:0.1)F;",# 8) a tree rooted on a leaf node (rare)
-]
+using Base.CoreLogging:Debug
 
+@testset "Newick" begin
 
-@testset "popular" begin
-    stream = TranscodingStreams.NoopStream(IOBuffer(newicks[6]))
+@testset "no nodes are named" begin
+
+    #         12345678
+    newick = "(,,(,));"
+
+    stream = TranscodingStreams.NoopStream(IOBuffer(newick))
 
     root = Newick.Record()
-    record = root
 
-    @test !Newick.haschildren(record)
+    @test_logs(
+        (:debug, "cladestart 1"),
+        (:debug, "nodefinish 2"),
+        (:debug, "nodefinish 3"),
+        (:debug, "cladestart 4"),
+        (:debug, "nodefinish 5"),
+        (:debug, "nodefinish 6"),
+        (:debug, "cladefinish 6"),
+        (:debug, "nodefinish 7"),
+        (:debug, "cladefinish 7"),
+        (:debug, "finish 8"),
+        min_level=Debug,
+        Newick.read!(stream, root)
+    )
 
-    # @test_logs (:debug, "nodeliststart") min_level=Base.CoreLogging.Debug Newick.readrecord!(stream, root, (1,1))
-    @test_logs (:debug, "nodeliststart 1") min_level=Base.CoreLogging.Debug Newick.readrecord!(stream, record)
+    @info "no nodes are named" newick root
 
-    @test Newick.haschildren(record)
-    parent = record
-
-    record = Newick.Record(parent)
-    @test_logs (:debug, "leafname 3") (:debug, "leafdistance 7") (:debug, "leaffinish 7") min_level=Base.CoreLogging.Debug Newick.readrecord!(stream, record)
-
-    push!(root.children, record)
-
-    record = Newick.Record(root)
-    @test_logs (:debug, "leafname 22") (:debug, "leafdistance 26") (:debug, "leaffinish 13") min_level=Base.CoreLogging.Debug Newick.readrecord!(stream, record)
-
-    push!(root.children, record)
-
-    record = Newick.Record(root)
-    @test_logs (:debug, "nodeliststart 14") min_level=Base.CoreLogging.Debug Newick.readrecord!(stream, root)
-
-    push!(root.children, record)
-
-    parent = record
-    record = Newick.Record(parent)
-    @test_logs (:debug, "leafname 3") (:debug, "leafdistance 7") (:debug, "leaffinish 7") min_level=Base.CoreLogging.Debug Newick.readrecord!(stream, record)
-    push!(root.children, record)
-
-    record = Newick.Record(parent)
-    @test_logs (:debug, "leafname 3") (:debug, "leafdistance 7") (:debug, "leaffinish 7") min_level=Base.CoreLogging.Debug Newick.readrecord!(stream, record)
-    push!(root.children, record)
-
+    Newick.print_tree(root)
 
 end
+
+@testset "leaf nodes are named" begin
+
+    #         000000000111
+    #         123456789012
+    newick = "(A,B,(C,D));"
+
+    stream = TranscodingStreams.NoopStream(IOBuffer(newick))
+
+    root = Newick.Record()
+
+    @test_logs(
+        (:debug, "cladestart 1"),
+        (:debug, "name 3"),
+        (:debug, "nodefinish 3"),
+        (:debug, "name 5"),
+        (:debug, "nodefinish 5"),
+        (:debug, "cladestart 6"),
+        (:debug, "name 8"),
+        (:debug, "nodefinish 8"),
+        (:debug, "name 10"),
+        (:debug, "nodefinish 10"),
+        (:debug, "cladefinish 10"),
+        (:debug, "nodefinish 11"),
+        (:debug, "cladefinish 11"),
+        (:debug, "finish 12"),
+        min_level=Debug,
+        Newick.read!(stream, root)
+    )
+
+    @info "leaf nodes are named" newick root
+
+    Newick.print_tree(root)
+
+end
+
+@testset "all nodes are named" begin
+    #         00000000011111
+    #         12345678901234
+    newick = "(A,B,(C,D)E)F;"
+
+    stream = TranscodingStreams.NoopStream(IOBuffer(newick))
+
+    root = Newick.Record()
+
+    @test_logs(
+        (:debug, "cladestart 1"),
+        (:debug, "name 3"), #A
+        (:debug, "nodefinish 3"),
+        (:debug, "name 5"), #B
+        (:debug, "nodefinish 5"),
+        (:debug, "cladestart 6"),
+        (:debug, "name 8"), #C
+        (:debug, "nodefinish 8"),
+        (:debug, "name 10"), #D
+        (:debug, "nodefinish 10"),
+        (:debug, "cladefinish 10"),
+        (:debug, "name 12"), #E
+        (:debug, "nodefinish 12"),
+        (:debug, "cladefinish 12"),
+        (:debug, "name 14"), #F
+        (:debug, "finish 14"),
+        min_level=Debug,
+        Newick.read!(stream, root)
+    )
+
+    @info "all nodes are named" newick root
+
+    Newick.print_tree(root)
+
+end
+
+@testset "all but root node have a distance to parent" begin
+
+    #         0000000001111111111222222222
+    #         1234567890123456789012345678
+    newick = "(:0.1,:0.2,(:0.3,:0.4):0.5);"
+
+    stream = TranscodingStreams.NoopStream(IOBuffer(newick))
+
+    root = Newick.Record()
+
+    @test_logs(
+        (:debug, "cladestart 1"),
+        (:debug, "distance 6"),
+        (:debug, "nodefinish 6"),
+        (:debug, "distance 11"),
+        (:debug, "nodefinish 11"),
+        (:debug, "cladestart 12"),
+        (:debug, "distance 17"),
+        (:debug, "nodefinish 17"),
+        (:debug, "distance 22"),
+        (:debug, "nodefinish 22"),
+        (:debug, "cladefinish 22"),
+        (:debug, "distance 27"),
+        (:debug, "nodefinish 27"),
+        (:debug, "cladefinish 27"),
+        (:debug, "finish 28"),
+        min_level=Debug,
+        Newick.read!(stream, root)
+    )
+
+    @info "all but root node have a distance to parent" newick root
+
+    Newick.print_tree(root)
+
+end
+
+@testset "all have a distance to parent" begin
+
+    #         00000000011111111112222222222333
+    #         12345678901234567890123456789012
+    newick = "(:0.1,:0.2,(:0.3,:0.4):0.5):0.0;"
+
+    stream = TranscodingStreams.NoopStream(IOBuffer(newick))
+
+    root = Newick.Record()
+
+    @test_logs(
+        (:debug, "cladestart 1"),
+        (:debug, "distance 6"),
+        (:debug, "nodefinish 6"),
+        (:debug, "distance 11"),
+        (:debug, "nodefinish 11"),
+        (:debug, "cladestart 12"),
+        (:debug, "distance 17"),
+        (:debug, "nodefinish 17"),
+        (:debug, "distance 22"),
+        (:debug, "nodefinish 22"),
+        (:debug, "cladefinish 22"),
+        (:debug, "distance 27"),
+        (:debug, "nodefinish 27"),
+        (:debug, "cladefinish 27"),
+        (:debug, "distance 32"),
+        # (:debug, "nodefinish 32"), #TODO: not sure about this one. But, it does yeild the correct result.
+        (:debug, "finish 32"),
+        min_level=Debug,
+        Newick.read!(stream, root)
+    )
+
+    @info "all have a distance to parent" newick root
+
+    Newick.print_tree(root)
+
+end
+
+@testset "distances and leaf names (popular)" begin
+
+    #         00000000011111111112222222222333
+    #         12345678901234567890123456789012
+    newick = "(A:0.1,B:0.2,(C:0.3,D:0.4):0.5);"
+
+    stream = TranscodingStreams.NoopStream(IOBuffer(newick))
+
+    root = Newick.Record()
+
+    @test_logs(
+        (:debug, "cladestart 1"),
+        (:debug, "name 3"),
+        (:debug, "distance 7"),
+        (:debug, "nodefinish 7"),
+        (:debug, "name 9"),
+        (:debug, "distance 13"),
+        (:debug, "nodefinish 13"),
+        (:debug, "cladestart 14"),
+        (:debug, "name 16"),
+        (:debug, "distance 20"),
+        (:debug, "nodefinish 20"),
+        (:debug, "name 22"),
+        (:debug, "distance 26"),
+        (:debug, "nodefinish 26"),
+        (:debug, "cladefinish 26"),
+        (:debug, "distance 31"),
+        (:debug, "nodefinish 31"),
+        (:debug, "cladefinish 31"),
+        (:debug, "finish 32"),
+        min_level=Debug,
+        Newick.read!(stream, root)
+    )
+
+    @info "distances and leaf names (popular)" newick root
+
+    Newick.print_tree(root)
+
+end
+
+@testset "distances and all names" begin
+
+    #         0000000001111111111222222222233333
+    #         1234567890123456789012345678901234
+    newick = "(A:0.1,B:0.2,(C:0.3,D:0.4)E:0.5)F;"
+
+    stream = TranscodingStreams.NoopStream(IOBuffer(newick))
+
+    root = Newick.Record()
+
+    @test_logs(
+        (:debug, "cladestart 1"),
+        (:debug, "name 3"),
+        (:debug, "distance 7"),
+        (:debug, "nodefinish 7"),
+        (:debug, "name 9"),
+        (:debug, "distance 13"),
+        (:debug, "nodefinish 13"),
+        (:debug, "cladestart 14"),
+        (:debug, "name 16"),
+        (:debug, "distance 20"),
+        (:debug, "nodefinish 20"),
+        (:debug, "name 22"),
+        (:debug, "distance 26"),
+        (:debug, "nodefinish 26"),
+        (:debug, "cladefinish 26"),
+        (:debug, "name 28"),
+        (:debug, "distance 32"),
+        (:debug, "nodefinish 32"),
+        (:debug, "cladefinish 32"),
+        (:debug, "name 34"),
+        (:debug, "finish 34"),
+        min_level=Debug,
+        Newick.read!(stream, root)
+    )
+
+    @info "distances and all names" newick root
+
+    Newick.print_tree(root)
+
+end
+
+@testset "a tree rooted on a leaf node (rare)" begin
+
+    #         00000000011111111112222222222333333
+    #         12345678901234567890123456789012345
+    newick = "((B:0.2,(C:0.3,D:0.4)E:0.5)A:0.1)F;"
+
+    stream = TranscodingStreams.NoopStream(IOBuffer(newick))
+
+    root = Newick.Record()
+
+    @test_logs(
+        (:debug, "cladestart 1"),
+        (:debug, "cladestart 2"),
+        (:debug, "name 4"),
+        (:debug, "distance 8"),
+        (:debug, "nodefinish 8"),
+        (:debug, "cladestart 9"),
+        (:debug, "name 11"),
+        (:debug, "distance 15"),
+        (:debug, "nodefinish 15"),
+        (:debug, "name 17"),
+        (:debug, "distance 21"),
+        (:debug, "nodefinish 21"),
+        (:debug, "cladefinish 21"),
+        (:debug, "name 23"),
+        (:debug, "distance 27"),
+        (:debug, "nodefinish 27"),
+        (:debug, "cladefinish 27"),
+        (:debug, "name 29"),
+        (:debug, "distance 33"),
+        (:debug, "nodefinish 33"),
+        (:debug, "cladefinish 33"),
+        (:debug, "name 35"),
+        (:debug, "finish 35"),
+        min_level=Debug,
+        Newick.read!(stream, root)
+    )
+
+    @info "a tree rooted on a leaf node (rare)" newick root
+
+    Newick.print_tree(root)
+
+end
+
+end # testset "Newick"
